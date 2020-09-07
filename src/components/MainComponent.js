3d8d93a7281957cd5ad5ecfdf0cf35d2e330d440
JamshyEK <ekmjamshy@gmail.com>
2020-09-07 19:28:14 +0530

    second assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import {DISHES} from '../shared/dishes';
 import {PROMOTIONS} from '../shared/promotions';
@@ -60,6 +61,7 @@ class Main extends Component {
           <Route path="/home" component={HomeComponent}/>
           <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
           <Route path="/menu/:dishId" component={dishWithId}/>
+          <Route exact path="/aboutus" component={()=><About leaders={this.state.leaders}/>}/>
           <Route exact path="/contactus" component={Contact}/>
           <Redirect to="/home"/>
         </Switch>
